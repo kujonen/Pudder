4946df7e9faf71e025b54c7bdffab781862610fe
Jon Sandvand <kujonen@gmail.com>
2014-04-16 13:14:52 +0200

    Forbedret utregning av temperatur

--- PudderVarsel.Dat/WeatherData.cs
+++ PudderVarsel.Dat/WeatherData.cs
@@ -11,6 +11,7 @@ namespace PudderVarsel.Data
     public class WeatherData
     {
         public string locationsString { get; set; }
+        const string Format = "yyyy-MM-ddTHH:mm:ssZ";
 
         public IEnumerable<Lokasjon> GetLocationForecast(double currentLat, double currentLon, string locations, int maxDistance, string searchText)
         {
@@ -64,41 +65,33 @@ namespace PudderVarsel.Data
             currentLocation.Distance = 0;
             locations[locations.Length-1] = currentLocation;
             return locations;
-            
         }
 
 
         public DagligPuddervarsel[] ProcessResponse(XElement forecastResponse)
-        //public string ProcessResponse(XElement forecastResponse)
         {
-            string testText = string.Empty;
+            var timeInfo = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
+            var ciNo = new CultureInfo("nb-NO");
             var items = forecastResponse.DescendantsAndSelf("time");
 
             var xElements = items as XElement[] ?? items.ToArray();
             var powderForecastDays = new DagligPuddervarsel[42];
             var i = 0;
+
+            var temperatureList = new Dictionary<string, string>();
             foreach (var xElement in xElements)
             {
-                var from = XmlHelper.GetAttributeValue("from", xElement);
-                var to = XmlHelper.GetAttributeValue("to", xElement);
+                var fromDateTime = TimeZoneInfo.ConvertTime(DateTime.ParseExact(XmlHelper.GetAttributeValue("from", xElement), Format, ciNo), TimeZoneInfo.Local, timeInfo).ToUniversalTime();
+                var toDateTime = TimeZoneInfo.ConvertTime(DateTime.ParseExact(XmlHelper.GetAttributeValue("to", xElement), Format, ciNo), TimeZoneInfo.Local, timeInfo).ToUniversalTime();
 
-                var format = "yyyy-MM-ddTHH:mm:ssZ";
-                var ciNo = new CultureInfo("nb-NO");
-                var fromDateTime = DateTime.ParseExact(from, format, ciNo);
-                var toDateTime = DateTime.ParseExact(to, format, ciNo);
-
-                var timeInfo = TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time");
-                var fra = TimeZoneInfo.ConvertTime(fromDateTime, TimeZoneInfo.Local, timeInfo).ToUniversalTime();
-                var til = TimeZoneInfo.ConvertTime(toDateTime, TimeZoneInfo.Local, timeInfo).ToUniversalTime();
-
-
-                //var hours = (toDateTime - fromDateTime).TotalHours;
-                //if (hours == 6)
-                //    testText += "Fra: " + fromDateTime + " Til: " + toDateTime + Environment.NewLine;
-
-                if (IsRelevant(fra, til))
+                var temperature = XmlHelper.GetElementValue("location", "temperature", "value", xElement);
+                if (!string.IsNullOrEmpty(temperature))
                 {
-                    
+                    temperatureList.Add(fromDateTime.ToString(), temperature);
+                }
+
+                if (IsRelevant(fromDateTime, toDateTime))
+                {
                     var powderForecast = new DagligPuddervarsel();
 
                     var precipitation = XmlHelper.GetElementValue("location", "precipitation", "value", xElement);
@@ -106,8 +99,8 @@ namespace PudderVarsel.Data
                     powderForecast.From = fromDateTime;
                     powderForecast.To = toDateTime;
 
-                    powderForecast.Temperature = GetAverageTemp(fromDateTime, toDateTime, xElements);
-
+                    powderForecast.Temperature = GetAverageTemp(temperatureList, ciNo);
+                    temperatureList = new Dictionary<string, string>();
                     powderForecastDays[i] = powderForecast;
                     i++;
                 }
@@ -116,88 +109,42 @@ namespace PudderVarsel.Data
             //return testText;
         }
 
-        private decimal GetAverageTemp(DateTime fromDateTime, DateTime toDateTime, XElement[] forecast)
+        private static decimal GetAverageTemp(Dictionary<string,string> temperatureList, CultureInfo ciNo)
         {
-            decimal temp = 0;
-            var hits = 0;
-            foreach (var xElement in forecast)
-            {
-                var from = XmlHelper.GetAttributeValue("from", xElement);
-                var to = XmlHelper.GetAttributeValue("to", xElement);
-                const string format = "yyyy-MM-ddTHH:mm:ssZ";
-                var ciNo = new CultureInfo("nb-NO");
-                var f = DateTime.ParseExact(from, format, ciNo);
-                var t = DateTime.ParseExact(to, format, ciNo);
-                if (f >= fromDateTime && t <= toDateTime)
-                {
-                    var tem = XmlHelper.GetElementValue("location", "temperature", "value", xElement);
-                    if (!string.IsNullOrEmpty(tem))
-                    {
-                        temp += Convert.ToDecimal(tem.Replace('.', ','), ciNo);
-                        hits++;
-                    }
-                }
-            }
-            var average = temp/hits;
+            var temp = temperatureList.Sum(temperature => Convert.ToDecimal(temperature.Value.Replace('.', ','), ciNo));
+
+            var average = temp/temperatureList.Count;
             return Math.Round(average, 1);
-
-
         }
 
-        private bool IsRelevant(DateTime from, DateTime to)
+        private static bool IsRelevant(DateTime from, DateTime to)
         {
             var longDate = DateTime.Now.AddDays(3).AddHours(-DateTime.Now.Hour);
             if (from < longDate)
             {
-                ////Amrikansk
-                //if (from.ToString().Contains("AM") || from.ToString().Contains("PM"))
-                //{
-                //    if (from.Hour == 10 && to.Hour == 4)
-                //        return true;
-                //    if (from.Hour == 4 && to.Hour == 10)
-                //        return true;
-                //}
-                //else
-                //{
-                    //Norsk
-                    if (from.Hour == 12 && to.Hour == 18)
-                        return true;
-
-                    if (from.Hour == 0 && to.Hour == 6)
-                        return true;
-
-                    if (from.Hour == 6 && to.Hour == 12)
-                        return true;
-
-                    if (from.Hour == 18 && to.Hour == 0)
-                        return true;
-                //}
+                if (from.Hour == 12 && to.Hour == 18)
+                    return true;
+                if (from.Hour == 0 && to.Hour == 6)
+                    return true;
+                if (from.Hour == 6 && to.Hour == 12)
+                    return true;
+                if (from.Hour == 18 && to.Hour == 0)
+                    return true;
             }
             else
             {
-                //if (from.ToString().Contains("AM") || from.ToString().Contains("PM"))
-                //{
-                //    //Amrikansk
-                //    if (from.Hour == 12 && to.Hour == 6)
-                //        return true;
-                //    if (from.Hour == 6 && to.Hour == 12)
-                //        return true;
-                //}
-                //else
-                //{
-                    //Norsk
-                    if (from.Hour == 2 && to.Hour == 8)
-                        return true;
-                    if (from.Hour == 8 && to.Hour == 14)
-                        return true;
-                    if (from.Hour == 14 && to.Hour == 20)
-                        return true;
-                    if (from.Hour == 20 && to.Hour == 2)
-                        return true;
-                //}
+                if (from.Hour == 2 && to.Hour == 8)
+                    return true;
+                if (from.Hour == 8 && to.Hour == 14)
+                    return true;
+                if (from.Hour == 14 && to.Hour == 20)
+                    return true;
+                if (from.Hour == 20 && to.Hour == 2)
+                    return true;
             }
             return false;
         }
+
         private double GetDistance(double currentLat, double currentLong, double locationLat, double locationLong)
         {
             //radians = degrees * PI / 180
@@ -219,55 +166,6 @@ namespace PudderVarsel.Data
         {
             return degrees * (Math.PI / 180);
         }
-        //private void GetPowder(IEnumerable<LocationPowderForecast> powderData)
-        //{
-        //    powderResult.DataSource = powderData;
-        //    powderResult.DataBind();
-        //}
-
-
-        //public static async void GetPowderData(string lat, string lon)
-        //{
-        //    try
-        //    {
-
-        //        HttpClient client = new HttpClient();
-        //        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/xml"));
-        //        var searchRequestUrl = "http://api.met.no/weatherapi/locationforecast/1.8/?lat=" + lat + ";" + "lon=" + lon;
-        //        HttpResponseMessage response = await client.GetAsync(searchRequestUrl);
-        //        var responseString = response.Content.ReadAsStringAsync();
-        //        //responseString.
-        //        //using (var client = new WebClient())
-        //        //{
-        //        //    client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
-
-
-        //        //    //"lat=60.10;lon=9.58";
-
-        //        //    var request = WebRequest.Create(searchRequestUrl) as HttpWebRequest;
-        //        //    if (request != null)
-        //        //    {
-        //        //        var response = request.GetResponse() as HttpWebResponse;
-        //        //        var respondse = request.() as HttpWebResponse;
-
-        //        //        var xmlDoc = new XmlDocument();
-        //        //        if (response != null) xmlDoc.Load(response.GetResponseStream());
-
-        //        //        responseWeather = ProcessResponse(xmlDoc);
-        //        //    }
-        //        //}
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        // Log error here
-        //        // Allow exception to bubble up
-
-        //        throw ex;
-        //    }
-
-        //    //return responseWeather;
-        //}
-
     }
 
 }
